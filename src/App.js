551e2135c4a6865b15d83bd5b46ead119443b1f2
freetimezp <kraftukrnet@ukr.net>
2023-11-15 23:23:52 +0200

    02. js-cars-06. Fetch data from api.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'boxicons/css/boxicons.min.css';
 import './App.css';
 
+import Banner from './pages/Banner';
+
+export const AppContext = React.createContext();
+
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch('http://localhost:3000/api/vehiclesData.json')
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(err => console.log(err.message));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  //console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppContext.Provider value={{ data, setData }}>
+        <Banner />
+      </AppContext.Provider>
+    </>
   );
 }
 
